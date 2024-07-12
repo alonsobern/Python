eff7fa02d4db8665c7d3add04d6632e392a317e4
Manuel Alonso Bernabe <alonso.bernabeb@gmail.com>
2024-07-12 22:40:34 +0100

    12.07.2024 - 22:40

--- Airbnb Project/Geospatial Insights/test.py
+++
@@ -1,6 +0,0 @@
-import geopandas as gpd
-import geodatasets
-
-dublin = gpd.read_file(geodatasets.get_path('geoda.nepal'))
-dublin = dublin.rename(columns={"name_2": "zone"})
-#print(dublin[["zone", "geometry"]].head())
